d89de5fc15abebb7d35809b5bed582d59ec55702
Jakub <60443955+jakubkobus@users.noreply.github.com>
2024-12-17 04:41:56 +0100

    new: labor6/zad1

---
+++ labor6/zad1/zad1.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+void printArray(int *arr, int n) {
+  for(int i = 0; i < n; i++)
+    printf("%d ", arr[i]);
+  printf("\n");
+}
+
+bool nextPermutation(int *arr, int n, int min, int max) {
+  int j;
+  for(int i = n - 1; i >= 0; i--)
+    if(arr[i]++ < max) {
+      for(j = i + 1; j < n; j++)
+        arr[j] = min;
+
+      return true;
+    }
+  
+  return false;
+}
+
+bool isValid(int *arr, int n) {
+  int *rows = (int *)calloc(n + 1, sizeof(int));
+  int *diag1 = (int *)calloc(2 * n, sizeof(int));
+  int *diag2 = (int *)calloc(2 * n, sizeof(int));
+
+  if(rows == NULL || diag1 == NULL || diag2 == NULL) {
+    perror("Memory allocation error");
+    free(rows);
+    free(diag1);
+    free(diag2);
+    return false;
+  }
+
+  bool valid = true;
+  for(int i = 0; i < n && valid; i++) 
+    if(++rows[arr[i]] > 1 || 
+       ++diag1[i - arr[i] + n] > 1 || 
+       ++diag2[i + arr[i]] > 1) 
+      valid = false;
+
+  free(rows);
+  free(diag1);
+  free(diag2);
+  return valid;
+}
+
+int main(int argc, char *argv[]) { 
+  if(argc != 2) {
+    printf("Unknown usage\n");
+    return 1;
+  }
+
+  int n = atoi(argv[1]);
+
+  int *arr = (int *)malloc(n * sizeof(int));
+  if(arr == NULL) {
+    perror("Memory allocation error");
+    free(arr);
+    return 1;
+  }
+
+  for(int i = 0; i < n; i++)
+    arr[i] = 1;
+  
+  int validSolutions = 0;
+
+  do {
+    if(isValid(arr, n)) {
+      printArray(arr, n);
+      validSolutions++;
+    }
+  } while(nextPermutation(arr, n, 1, n));
+
+  printf("Number of solutions: %d\n", validSolutions);
+
+  free(arr);
+  return 0;
+}
